de8157873f403b1b6053e247e2a0bd87fab83ca4
neofytr <imagination12344321.com@gmail.com>
2025-02-02 12:56:49 +0530

    network.h

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main(int argc, char **argv)
 {
-    printf("hello, world");
+    
     return EXIT_SUCCESS;
 }
---
+++ network.h
@@ -0,0 +1,19 @@
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+#define _GNU_SOURCE
+
+/*  
+
+Common includes for network programming
+
+*/
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+#endif 
