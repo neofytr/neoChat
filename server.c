1c7580b0c3a288c7c5f0e7308413863eb76f9a51
neofytr <imagination12344321.com@gmail.com>
2025-02-02 13:28:17 +0530

    thread pool for request handling

--- server.c
+++ server.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <stdbool.h>
 
 #include "network.h"
 
 #define CHAT_PORT "4040"
+#define LISTEN_BUF_LEN (100)
+
+#define THREAD_POOL_SIZE (16)
+
+pthread_t thread_pool[THREAD_POOL_SIZE];
+
+void *thread_function(void *arg);
 
 void print_getaddrinfo(struct addrinfo *servinfo)
 {
@@ -45,12 +54,64 @@ int main(int argc, char **argv)
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
 
+    pthread_attr_t attr;
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+
+    for (size_t counter = 0; counter < THREAD_POOL_SIZE; counter++)
+    {
+        if (pthread_create(&thread_pool[counter], &attr, thread_function, NULL))
+        {
+            pthread_attr_destroy(&attr);
+            perror("pthread_create");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    pthread_attr_destroy(&attr);
+
     if (getaddrinfo(NULL, CHAT_PORT, &hints, &server))
     {
         perror("getaddrinfo");
         exit(EXIT_FAILURE);
     }
 
+    int server_fd = socket(server->ai_family, server->ai_socktype, server->ai_protocol);
+    if (server_fd == -1)
+    {
+        freeaddrinfo(server);
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
 
+    int yes = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1)
+    {
+        perror("setsockopt");
+        close(server_fd);
+        freeaddrinfo(server);
+        exit(EXIT_FAILURE);
+    }
+
+    if ((bind(server_fd, (struct sockaddr *)server->ai_addr, server->ai_addrlen)) == -1)
+    {
+        close(server_fd);
+        freeaddrinfo(server);
+        perror("bind");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((listen(server_fd, LISTEN_BUF_LEN)) == -1)
+    {
+        close(server_fd);
+        freeaddrinfo(server);
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    while (true)
+    {
+    }
+
+    close(server_fd);
     return EXIT_SUCCESS;
 }
